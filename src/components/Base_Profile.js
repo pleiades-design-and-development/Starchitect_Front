4f237cd7489fd7067fef7d47b34864851e6a0846
JP Ubana <jpjubana@gmail.com>
2017-08-17 17:37:30 -0400

    fixed some dimensioning issues

--- src/components/Base_Profile.js
+++ src/components/Base_Profile.js
@@ -15,27 +15,27 @@ export default class Base_Profile extends React.Component {
   render() {
     return (
       <div className='profile-container'>
-        <div id='sidebar'>
+        <div id='sidebar' style={{width: '20vw', height: '100vh;'}}>
           <Menu vertical id='menu'>
             <img src='profile-image.jpeg' alt='profile image' className='profile-image'/>
+            <p>Brandon Stark</p>
             <Menu.Item name='rank' id='rank'>
-              <img src='cadetBadge.svg' alt='rank badge' />
-              <p>Cadet</p>
+              <img src='cadetBadge.svg' alt='rank badge'/>
+              <p style={{marginTop: '10px', marginLeft: '5px'}}>CADET</p>
             </Menu.Item>
             <Menu.Item name='progress-bar' id='progress-bar'>
               <Progress percent='30' indicating />
             </Menu.Item>
             <Menu.Item name='mode' id='mode'>
               <Button id='label'>Explorer</Button>
-              <span>||</span>
               <Button id='label'>Creator</Button>
             </Menu.Item>
             <Menu.Item name='last-location'>
-              <p className='head'>Last Known Location</p>
+              <p className='head'>Last Known Location:</p>
               <p>Jupiter</p>
             </Menu.Item>
             <Menu.Item name='beacons'>
-              <p className='head'>Beacons</p>
+              <p className='head'>Beacons:</p>
               <p>Jupiter</p>
               <p>Io</p>
               <p>Europa</p>
@@ -48,7 +48,7 @@ export default class Base_Profile extends React.Component {
             <Menu.Item name='crew-lounge'>
               <Link to='' className='link-color'>Crew Lounge</Link>
             </Menu.Item>
-            <Button><a href={'./Starmap'}>Return to Starmap</a></Button>
+            <Button style={{marginBottom: '1em'}}><a href={'./Starmap'}>Return to Starmap</a></Button>
           </Menu>
         </div>
         <div className="fullscreen-bg">
