ccaeb0f4e6c8198df7070d3d9c42b6967af99fad
bmdilts <bmdilts@gmail.com>
2017-08-18 15:33:51 -0400

    crew lounge scaffold started

--- src/components/App_Profile.js
+++ src/components/App_Profile.js
@@ -4,8 +4,9 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Base_Profile from './Base_Profile';
 import FeedPage from './FeedPage';
-import Beacons from './Beacons'
-import Submissions from './Submissions'
+import Beacons from './Beacons';
+import Submissions from './Submissions';
+import Crew_Lounge from './Crew_Lounge';
 
 export default class App_Profile extends React.Component {
   constructor(props) {
@@ -21,6 +22,7 @@ export default class App_Profile extends React.Component {
               <Route path='/Profile' component={FeedPage}></Route>
               <Route path='/Beacons' component={Beacons}></Route>
               <Route path='/Submissions' component={Submissions}></Route>
+              <Route path='/CrewLounge' component={Crew_Lounge}></Route>
             </Switch>
           </Base_Profile>
         </BrowserRouter>
