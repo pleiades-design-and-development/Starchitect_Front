06ecd01256bd8a262accc3b5509577ec71b26e9d
JP Ubana <jpjubana@gmail.com>
2017-08-25 09:23:52 -0400

    starmap about to be lit

--- src/components/Starmap.js
+++ src/components/Starmap.js
@@ -10,10 +10,67 @@ export default class Starmap extends React.Component {
   render() {
     return (
       <div style={{height: '80vh'}}>
-        <Link to='/Sun'><img id='sun' style={{width: '20em'}} src="Sun.png" alt="sun"/></Link>
-        <Link to='/Earth'><img className='planets earth' src="p-earth.png" alt="earth"/></Link>
-        <Link to='/Jupiter'><img className='planets jupiter' src="p-jupiter.png" alt="jupiter"/></Link>
-        <Link to='/Mars'><img className='planets mars' src="p-mars.png" alt="mars"/></Link>
+        <div id="mercury" className="orbit">
+          <div className="pos">
+            <div className="planet"></div>
+          </div>
+        </div>
+
+        <div id="venus" className="orbit">
+          <div className="pos">
+            <div className="planet"></div>
+          </div>
+        </div>
+
+        <div id="earth" className="orbit">
+          <div className="pos">
+            <div className="orbit">
+                <div className="pos">
+                  <div className="moon"></div>
+                </div>
+            </div>
+            <div className="planet"></div>
+          </div>
+        </div>
+                
+        <div id="mars" className="orbit">
+          <div className="pos">
+            <div className="planet"></div>
+          </div>
+        </div>
+
+        <div id="jupiter" className="orbit">
+          <div className="pos">
+            <div className="planet"></div>
+          </div>
+        </div>
+
+        <div id="saturn" className="orbit">
+          <div className="pos">
+            <div className="planet">
+              <div className="ring">
+            </div>
+          </div>
+        </div>
+
+        <div id="uranus" className="orbit">
+          <div className="pos">
+            <div className="planet"></div>
+          </div>
+        </div>
+
+        <div id="neptune" className="orbit">
+          <div className="pos">
+            <div className="planet"></div>
+          </div>
+        </div>
+
+        <div id="sun"></div>
+
+        {/*<Link to='/Sun'><img id='sun' style={{width: '20em'}} src="Sun.png" alt="sun"/></Link>
+        <Link to='/Earth'><img classNameName='planets earth' src="p-earth.png" alt="earth"/></Link>
+        <Link to='/Jupiter'><img classNameName='planets jupiter' src="p-jupiter.png" alt="jupiter"/></Link>
+        <Link to='/Mars'><img classNameName='planets mars' src="p-mars.png" alt="mars"/></Link>*/}
       </div>
     );
   }
