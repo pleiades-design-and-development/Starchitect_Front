ca9f8607c2c88ad904a71f3e4cf9edd09e50e89e
bmdilts <bmdilts@gmail.com>
2017-08-25 15:11:48 -0400

    fixed twinkling interference with wiki template youarehere images

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,6 +11,8 @@ export default class Login extends React.Component {
       callsign: '',
       password: '',
       redirect_profile: false,
+      error: false,
+      error_msg: '',
     };
   }
 
@@ -33,9 +35,14 @@ export default class Login extends React.Component {
       return data.json();
     }).then(response => {
       console.log(response, "yay");
-      sessionStorage.setItem('api_token', 'Token token=' + response.data.attributes['api-token']);
-      sessionStorage.setItem('userId', response.data.id);
-      this.setState({redirect_profile: true})
+      if(response.errors){
+        this.setState({ error_msg: response.errors[0].detail || ''})
+        this.setState({ error: true})
+      } else{
+        sessionStorage.setItem('api_token', 'Token token=' + response.data.attributes['api-token']);
+        sessionStorage.setItem('userId', response.data.id);
+        this.setState({redirect_profile: true})
+      }
     }).catch(err => {
       console.log(err, "boo!");
     });
@@ -43,24 +50,20 @@ export default class Login extends React.Component {
   }
 
   render() {
-    const { callsign, password, redirect_profile } = this.state
+    const { callsign, password, redirect_profile, error, error_msg } = this.state
     if (redirect_profile) {
       return <Redirect push to='/Profile'/>;
     }
     return (
       <div>
-        <Form size='big' key='big' onSubmit={this.handleSubmit} id='login'>
+        <Form size='big' key='big' onSubmit={this.handleSubmit} id='login' error={error}>
           <Form.Field id='form-input-control-callsign' name='callsign' value={callsign} control={Input} label='What does your squad call you?' placeholder='Callsign' onChange={this.handleChange} />
           <Form.Field id='form-input-control-password' name='password' type='password' value={password} control={Input} label='What is your high command authorization code?' placeholder='Password' onChange={this.handleChange} />
           <Message
-            success
-            header='Form Completed'
-            content="You are now enlisted in the Explorer Corps. Welcome Cadet!"
-          />
-          <Message
+            color='black'
             error
-            header='Action Forbidden'
-            content='You can only sign up for an account once with a given e-mail address.'
+            header='Houston, we have a problem!'
+            content={error_msg}
           />
           <Button type='submit'>Submit</Button>
         </Form>
