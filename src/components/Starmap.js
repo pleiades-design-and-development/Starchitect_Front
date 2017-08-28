1c2c62085a360943229ff31f6fac6f2fc50b37ea
bmdilts <bmdilts@gmail.com>
2017-08-28 15:13:21 -0400

    fixed merge conflicts

--- src/components/Starmap.js
+++ src/components/Starmap.js
@@ -11,20 +11,6 @@ export default class Starmap extends React.Component {
     return (
       <div className="set-speed view-2D zoom-close data-close controls-close">
         <div id="data">
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-          <Link to='/Sun'><a className="sun" title="sun">Sun</a></Link>
-          <Link to='/Mercury'><a className="mercury" title="mercury">Mercury</a></Link>
-          <Link to='/Venus'><a className="venus" title="venus">Venus</a></Link>
-          <Link to='/Earth'><a className="earth" title="earth">Earth</a></Link>
-          <Link to='/Mars'><a className="mars" title="mars">Mars</a></Link>
-          <Link to='/Jupiter'><a className="jupiter" title="jupiter">Jupiter</a></Link>
-          <Link to='/Saturn'><a className="saturn" title="saturn">Saturn</a></Link>
-          <Link to='/Uranus'><a className="uranus" title="uranus">Uranus</a></Link>
-          <Link to='/Neptune'><a className="neptune" title="neptune">Neptune</a></Link>
-=======
-=======
->>>>>>> Stashed changes
           <Link className='solar_system_link' className="sun" title="sun" to="/Sol">Sun</Link>
           <Link className='solar_system_link' className="mercury" title="mercury" to="/Mercury">Mercury</Link>
           <Link className='solar_system_link' className="venus" title="venus" to="/Venus">Venus</Link>
@@ -34,7 +20,6 @@ export default class Starmap extends React.Component {
           <Link className='solar_system_link' className="saturn" title="saturn" to="/Saturn">Saturn</Link>
           <Link className='solar_system_link' className="uranus" title="uranus" to="/Uranus">Uranus</Link>
           <Link className='solar_system_link' className="neptune" title="neptune" to="/Neptune">Neptune</Link>
->>>>>>> Stashed changes
         </div>
         <div id="universe" className="scale-stretched set-speed">
           <div id="galaxy">
@@ -84,15 +69,7 @@ export default class Starmap extends React.Component {
                   <Link to='/Neptune'><div className="planet"></div></Link>
                 </div>
               </div>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-              <Link to='/Sun'><div id="sun"></div></Link>
-=======
               <div id="sun"><Link to='/Sol' id='sun_link'></Link></div>
->>>>>>> Stashed changes
-=======
-              <div id="sun"><Link to='/Sol' id='sun_link'></Link></div>
->>>>>>> Stashed changes
             </div>
           </div>
         </div>
